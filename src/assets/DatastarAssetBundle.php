3ddc97f7832032797c21bc26643eb0e5f6ef29f5
Ben Croker <ben@putyourlightson.com>
2025-08-06 20:14:43 +0200

    1.0.0-RC.4

--- src/assets/DatastarAssetBundle.php
+++ src/assets/DatastarAssetBundle.php
@@ -7,7 +7,7 @@ use craft\web\AssetBundle;
 
 class DatastarAssetBundle extends AssetBundle
 {
-    public const VERSION = '1.0.0-RC.3';
+    public const VERSION = '1.0.0-RC.4';
 
     /**
      * @inheritdoc
