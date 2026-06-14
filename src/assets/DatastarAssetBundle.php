6981ad3c8c0c09d7af34665f36e4db96ff5f2fac
Ben Croker <ben@putyourlightson.com>
2026-06-14 22:21:52 +0200

    Include Datastar 1.0.2

--- src/assets/DatastarAssetBundle.php
+++ src/assets/DatastarAssetBundle.php
@@ -7,7 +7,7 @@ use craft\web\AssetBundle;
 
 class DatastarAssetBundle extends AssetBundle
 {
-    public const VERSION = '1.0.1';
+    public const VERSION = '1.0.2';
 
     /**
      * @inheritdoc
